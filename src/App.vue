8ba40cc4ec49ae4353a5db6f0dbda8767a7fc352
DiiasDev <devpdias@gmail.com>
2025-04-24 10:53:40 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <LoginVue />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {useAppStore} from './store/app.ts'
+import LoginVue from './components/login.vue'
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    LoginVue
+  },
+
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+  },
+  computed: {
+        appStore() {
+            return useAppStore();
+        },
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/login.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-sheet class="d-flex align-center justify-center" height="100vh">
+                <v-card width="30%">
+                    <v-card-title class="text-h5 text-center">
+                        Login
+                    </v-card-title>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="Email" type="email" required></v-text-field>
+                            <v-text-field label="Senha" type="password" required></v-text-field>
+                            <v-btn color="primary" class="mt-4">Entrar</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-sheet>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default{
+    name: 'LoginVue',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
