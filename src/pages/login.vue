6c9b796477ce42fdf19693d4a4f4b244dabf5f9d
DiiasDev <devpdias@gmail.com>
2025-05-21 15:38:18 -0300

    Finalização da tela de login.

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,19 +1,19 @@
 <template>
     <v-row class="no-scroll">
         <v-col class="d-flex align-center justify-center" cols="12">
-            <v-card rounded="lg" :width="display.xs ? '80%' : '30%'">
-                <v-card-title class="text-h5 text-center">
+            <v-card rounded="lg" :width="getCardWidth" :class="{'pa-2 ml-4': display.xs, 'pa-4': display.sm || display.md}">
+                <v-card-title class="text-h5 text-center" :class="{'text-h6': display.xs}">
                     Login
                 </v-card-title>
                 <v-card-text>
                     <v-form>
-                        <v-text-field label="Email" type="email" v-model="email" required></v-text-field>
-                        <v-text-field label="Senha" type="password" v-model="password" required></v-text-field>
-                        <v-btn color="primary" @click="isLoged" class="mt-4">Entrar</v-btn>
-                        <v-card-text>Ainda não tem login? Clique <strong style="cursor: pointer;"
+                        <v-text-field label="Email" type="email" v-model="email" required :density="display.xs ? 'compact' : 'default'"></v-text-field>
+                        <v-text-field label="Senha" type="password" v-model="password" required :density="display.xs ? 'compact' : 'default'"></v-text-field>
+                        <v-btn color="primary" @click="isLoged" class="mt-4" :block="display.xs || display.sm">Entrar</v-btn>
+                        <v-card-text class="px-0">Ainda não tem login? Clique <strong style="cursor: pointer;"
                                 @click="openModal">aqui</strong> para
                             cadastrar.</v-card-text>
-                        <v-card-text v-html="exibeMessage"></v-card-text>
+                        <v-card-text class="px-0" v-html="exibeMessage"></v-card-text>
                     </v-form>
                 </v-card-text>
             </v-card>
@@ -43,6 +43,12 @@ export default {
         appStore() {
             return useAppStore();
         },
+        getCardWidth() {
+            if (this.display.xs) return '90%'
+            if (this.display.sm) return '70%'
+            if (this.display.md) return '50%'
+            return '30%'
+        }
     },
     methods: {
         openModal() {
@@ -87,5 +93,15 @@ export default {
 
 html {
     overflow-y: hidden !important;
+}
+
+@media (max-width: 600px) {
+    .v-card-title {
+        margin-bottom: 0;
+    }
+    
+    .v-card-text {
+        padding-top: 8px;
+    }
 }
 </style>
